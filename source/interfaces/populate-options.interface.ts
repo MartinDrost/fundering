67dd68a93ee7737a0929d6964b657a7ef620910c
Martin Drost <info@martindrost.nl>
2022-02-03 10:57:35 +0100

    extended interface documentation

--- source/interfaces/populate-options.interface.ts
+++ source/interfaces/populate-options.interface.ts
@@ -1,10 +1,23 @@
 import { IQueryOptions } from "./query-options.interface";
 
+/**
+ * Options used for populating a model.
+ * All options from IHttpOptions are available and work as expected
+ * except for random, distinct, pipelines, session and maxTimeMS.
+ */
 export interface IPopulateOptions<ModelType = any>
   extends Omit<
     IQueryOptions<ModelType>,
-    "random" | "pipelines" | "session" | "maxTimeMS"
+    "random" | "distinct" | "pipelines" | "session" | "maxTimeMS"
   > {
+  /**
+   * The field to populate from the current level. Objects do not support dot notation.
+   * @example "school"
+   */
   path: string;
+
+  /**
+   * Recursive populate options used for deep population.
+   */
   populate?: IPopulateOptions<ModelType>[];
 }
--- source/interfaces/query-options.interface.ts
+++ source/interfaces/query-options.interface.ts
@@ -3,19 +3,96 @@ import { Conditions } from "../types/conditions.type";
 import { IPopulateOptions } from "./populate-options.interface";
 
 export interface IQueryOptions<ModelType = any> {
-  sort?: string[];
-  random?: boolean;
-  skip?: number;
-  limit?: number;
-  select?: string[];
-  distinct?: string;
-
+  /**
+   * Specifies the conditions which the documents must satisfy to be returned.
+   * Queries must satisfy rules set by the $match stage of mongodb aggregations.
+   * @example { name: { $in: ["John", "Jane"] } }
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/match/
+   */
   match?: Conditions<ModelType>;
+
+  /**
+   * Specifies the fields on which should be sorted.
+   * Fields can be prefixed with a hyphen (-) to sort in descending order.
+   * @example ["-lastName", "firstName"]
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+   */
+  sort?: string[];
+
+  /**
+   * Specifies the number of documents to skip.
+   * @example 5
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/skip/
+   */
+  skip?: number;
+
+  /**
+   * Specifies the number of documents to return.
+   * @example 10
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/limit/
+   */
+  limit?: number;
+
+  /**
+   * Specifies the fields to return in each model.
+   * The fields can be nested by using dot notation.
+   * @example ["name", "address.city"]
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/project/
+   */
+  select?: string[];
+
+  /**
+   * Specifies which fields should be populated on the returned models.
+   * The fields can be defined using strings with dot notation for nesting or
+   * by using population objects. The string notation is shorter but objects
+   * allow you to specify match queries, sort orders, limits etc.
+   * @example ['school.students']
+   * @example [{ path: 'school', populate: [{ path: 'students' }] }]
+   * @see https://mongoosejs.com/docs/populate.html
+   */
   populate?: (string | IPopulateOptions<ModelType>)[];
 
+  /**
+   * Specifies the field which you only want distinct (unique) values for.
+   * Inspired by SQL distinct.
+   * @example "firstName"
+   * @see https://www.w3schools.com/sql/sql_distinct.asp
+   */
+  distinct?: string;
+
+  /**
+   * Defines whether you want the result to be in a random order. Useful for
+   * batches of documents where you want to get a random sample of the results.
+   * This option overrules the sort option.
+   * @example true
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation/sample/
+   */
+  random?: boolean;
+
+  /**
+   * Extra aggregation pipelines which should be run after the main pipeline is finished.
+   * This is useful for manipulating the data structure of the result.
+   * @example [{ $addFields: { total: { $add: ["$price", "$tax"] } } }]
+   * @see https://docs.mongodb.com/manual/reference/operator/aggregation
+   */
   pipelines?: Record<string, any>[];
+
+  /**
+   * The transaction session to use for this query.
+   * @see https://mongoosejs.com/docs/api.html#query_Query-session
+   */
   session?: ClientSession;
+
+  /**
+   * The maximum amount of time to allow the query to run in milliseconds.
+   * @example 3000
+   * @default 10000
+   */
   maxTimeMS?: number;
 
+  /**
+   * All other fields which are not explicitly defined will be available in the options object
+   * but have no out-of-the-box functionality.
+   */
   [key: string]: any;
 }
