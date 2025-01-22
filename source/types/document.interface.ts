17425249b22de518f3c7ac6a74ef6d8407a9960a
Martin Drost <info@martindrost.nl>
2025-01-22 14:00:15 +0100

    removed undefined from document id types

--- source/types/document.interface.ts
+++ source/types/document.interface.ts
@@ -2,7 +2,7 @@ import { Document as MongooseDocument } from "mongoose";
 import { IModel } from "../interfaces/model.interface";
 import { IQueryOptions } from "../interfaces/query-options.interface";
 
-export type Document<ModelType = IModel> = MongooseDocument &
+export type Document<ModelType = IModel> = MongooseDocument<string | object> &
   ModelType & {
     $locals: {
       options?: IQueryOptions<ModelType>;
